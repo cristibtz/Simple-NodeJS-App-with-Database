b427116ae6f61eb8fb18afc45e7754def1b3f400
Cristian Branet <gbranet@dvloper.io>
2025-01-09 19:41:48 +0000

    Running the test app

--- Backend/index.js
+++ Backend/index.js
@@ -1,1 +1,17 @@
-console.log("hello");
+const express = require('express');
+
+const app = express();
+const PORT = 5000;
+
+app.get('/', (req, res)=>{
+    res.status(200);
+    res.send("Welcome to root URL of Server");
+});
+
+app.listen(PORT, '0.0.0.0',(error) =>{
+    if(!error)
+        console.log("Server is Successfully Running, and App is listening on port "+ PORT);
+    else 
+        console.log("Error occurred, server can't start", error);
+    }
+);
