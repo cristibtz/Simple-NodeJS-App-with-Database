3f170a257e564c3505af35053080047dcfdbbdbb
Cristian Branet <gbranet@dvloper.io>
2025-01-09 20:05:18 +0000

    we started

--- Backend/index.js
+++ Backend/index.js
@@ -1,11 +1,19 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 const PORT = 5000;
 
+app.use(express.static(path.join(__dirname, '../Frontend/views/')));
+
 app.get('/', (req, res)=>{
     res.status(200);
-    res.send("Welcome to root URL of Server");
+    res.sendFile(path.join(__dirname, '../Frontend/views/', 'index.html'));
+});
+
+app.get('/login', (req, res)=>{
+    res.status(200);
+    res.sendFile(path.join(__dirname, '../Frontend/views/', 'login.html'));
 });
 
 app.listen(PORT, '0.0.0.0',(error) =>{
